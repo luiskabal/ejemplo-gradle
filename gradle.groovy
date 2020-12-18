eec3fa026da4f29387ab1da4b9095288b458644b
LVaras <lvaras@aligare.cl>
2020-12-18 02:53:49 -0300

    qwe

--- gradle.groovy
+++ gradle.groovy
@@ -6,25 +6,30 @@
 
 def call(){
   
-	stage('build & test'){
+	stage('Build & Test'){
+		env.Tarea='Build & Test'
 		bat './gradlew clean build'
 		
 		}
 		
-	stage('sonar'){
+	stage('Sonar'){
+		env.Tarea='Sonar'
 					def scannerHome = tool 'sonar';
 					withSonarQubeEnv('sonar') {
 						bat "${scannerHome}\\bin\\sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
 					}
 		}
-	stage('run'){
+	stage('Run'){
+			env.Tarea='Run'
 			bat 'start gradlew bootRun'
 			sleep 7
 		}
-	stage('test'){
+	stage('Test'){
+		env.Tarea='Test'
 					bat "curl -X GET http://localhost:8082/rest/mscovid/test?msg=testing"
 		}
 	stage('Nexus Upload'){
+		env.Tarea='Nexus Upload'
 						nexusArtifactUploader(
 						nexusVersion: 'nexus3',
 						protocol: 'http',
