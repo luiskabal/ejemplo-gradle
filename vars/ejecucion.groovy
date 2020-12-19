45b575aeef9ef0b72988584bc41a81632f2c6cdd
LVaras <lvaras@aligare.cl>
2020-12-18 21:56:28 -0300

    asd

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -7,22 +7,17 @@ pipeline{
 			stage('Pipeline'){
 				steps{
 					script{
-					"${params.buildtool}".call()	
+					if(params.buildtool == 'gradle' ){
+					gradle.call();
+					}else{
+					maven.call();
+					}
 					}
 				}
 				
 			}
 	}
 
-   post {
-		failure {
-			slackSend channel: 'U01DK543PKN', color: 'danger', message: "[Luis Varas Q][${JOB_NAME}][${params.buildtool}] Ejecucion fallida en "+env.Tarea, teamDomain: 'dipdevopsusach2020', tokenCredentialId: 'slack-Token'
-		}
-        success {
-           slackSend channel: 'U01DK543PKN', color: 'good', message: "[Luis Varas Q][${JOB_NAME}][${params.buildtool}] Ejecucion exitosa "+env.Tarea, teamDomain: 'dipdevopsusach2020', tokenCredentialId: 'slack-Token'
-        }
-		
-    }
 }
 
 
